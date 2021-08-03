76a87462d4aadefa3598138a0b258b6870ce1e7e
zenithshrestha37 <zenithshrestha37@gmail.com>
2021-08-03 14:31:43 -0500

    arithmetic file updated

--- calc/Arithmetic.java
+++ calc/Arithmetic.java
@@ -2,34 +2,40 @@ package calc;
 
 public class Arithmetic {
 	public static float add(float num1, float num2) {
-		return 10.0f;
+		return num1 + num2;
 	}
 
 	public static float subtract(float num1, float num2) {
-		return 10.0f;
+
+		return num1 - num2;
 	}
 
 	public static float multiply(float num1, float num2) {
-		return 10.0f;
+
+		return num1 * num2;
 	}
 
 	public static float divide(float num1, float num2) {
-		return 10.0f;
+
+		return num1/num2;
 	}
 
 	public static float square(float num1) {
-		return 10.0f;
+
+		return num1*num1;
 	}
 
 	public static float cube(float num1) {
-		return 10.0f;
+
+		return num1*num1*num1;
 	}
 
 	public static float power(float num1, float num2) {
-		return 10.0f;
+
+		return (float) Math.pow(num1,num2);
 	}
 
 	public static float mod(float num1, float num2) {
-		return 10.0f;
+		return num1 % num2;
 	}
 }
